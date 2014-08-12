05aea69648f0bb6dbd9327606c0a4e6802ab1667
j5inc <nerojrnde@gmail.com>
2014-08-11 18:12:27 -0700

    Here we do, round two

--- app/src/main/java/com/j5inc/helloworld/MainActivity.java
+++ app/src/main/java/com/j5inc/helloworld/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
-    //this is a change
+    //this is a chand
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
